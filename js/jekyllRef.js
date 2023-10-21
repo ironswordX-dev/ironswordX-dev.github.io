18c1f4559bc9e5ab1d2ce191bb70df44219f77f0
ironswordX-dev <ironswordXstar@outlook.com>
2023-10-21 16:52:03 +0000

    eslintignore

--- js/jekyllRef.js
+++ js/jekyllRef.js
@@ -1,10 +1,10 @@
 let elem = document.createElement('div')
 let refElem = document.createElement('a')
 let imgElem = document.createElement('img')
-imgElem.src = "/assets/jekyllLogo.png";
+imgElem.src = '/assets/jekyllLogo.png';
 refElem.appendChild(imgElem);
-refElem.href = "https://jekyllrb.com/";
-elem.innerText = "Made & Deployed with ";
-elem.id = "jekyll";
+refElem.href = 'https://jekyllrb.com/';
+elem.innerText = 'Made & Deployed with ';
+elem.id = 'jekyll';
 elem.appendChild(refElem)
 document.body.appendChild(elem)
