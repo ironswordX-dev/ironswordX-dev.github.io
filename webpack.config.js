89193801dd8346a07cd2b882688a851a8c25fc34
ironswordX-dev <ironswordXstar@outlook.com>
2023-10-23 23:02:03 +0000

    k

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ...glob.sync('./node_modules/**/package.json').reduce((acc, path) => {
       const packageName = JSON.parse(fs.readFileSync(path)).name;
       const packageMain = JSON.parse(fs.readFileSync(path)).main;
-      acc[packageName] = `./node_modules/${packageName}/${packageMain}`;
+      acc[packageName] = `/node_modules/${packageName}/${packageMain}`;
       return acc;
     }, {}),
   },
